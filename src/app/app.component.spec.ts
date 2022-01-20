2e54846b7d748d14c7c7bbe03e605e660a89215f
NAKWONCHOIDESK1\NakWonCHOI <ch0494@gmail.com>
2022-01-20 22:49:16 +0900

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import { InboxScreenComponent } from './components/inbox-screen/inbox-screen.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen/pure-inbox-screen.component';
+import { TaskModule } from './components/task.module';
+import { TasksState } from './state/task.state';
 
 describe('App', () => {
+  let component: InboxScreenComponent;
+  let fixture: ComponentFixture<InboxScreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ 
+        PureInboxScreenComponent,
+        InboxScreenComponent,        
+      ],
+      imports: [
+        TaskModule,
+        NgxsModule.forRoot([TasksState]),
+      ],
+      providers: [
+        Store,
+      ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InboxScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
   test('should render the App component', async () => {
-    const { getByText } = await render(AppComponent);
-    expect(getByText('TaskList'));
+    expect(component).not.toBe(null);
   });
 })
